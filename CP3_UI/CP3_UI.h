895614912eec309c7f5ceb29813be0a30607f6ba
Corné <cdriesprong@gmail.com>
2020-04-16 18:15:47 +0200

    Initial Commit

---
+++ CP3_UI/CP3_UI.h
@@ -0,0 +1,19 @@
+//
+//  CP3_UI.h
+//  CP3_UI
+//
+//  Created by Corné on 16/04/2020.
+//  Copyright © 2020 cp3.io. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CP3_UI.
+FOUNDATION_EXPORT double CP3_UIVersionNumber;
+
+//! Project version string for CP3_UI.
+FOUNDATION_EXPORT const unsigned char CP3_UIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CP3_UI/PublicHeader.h>
+
+
